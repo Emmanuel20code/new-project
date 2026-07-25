af6ecdd7e138c3abfbf714a8d66dcecc6f54b53b
Emmanuel20code <emmanueloyaro3@gmail.com>
2026-07-25 23:53:42 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,10 @@
-import path from "node:path";
-import tailwindcss from "@tailwindcss/vite";
-import hercules from "@usehercules/vite";
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-    allowedHosts: true,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), tailwindcss(), hercules()],
-  resolve: {
-    alias: {
-      "@/convex": path.resolve(__dirname, "./convex"),
-      "@": path.resolve(__dirname, "./src"),
-    },
-    dedupe: [
-      "react",
-      "react-dom",
-      "react/jsx-runtime",
-      "react/jsx-dev-runtime",
-    ],
-  },
+  plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000,
-  },
-});
+    outDir: 'dist'
+  }
+})
